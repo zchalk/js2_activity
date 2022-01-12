80c9f329250abea35bf7516b9de24d93451e47ca
zchalk <you@example.com>
2022-01-12 09:55:00 -0500

    added activity content to repo for student forking

---
+++ script.js
@@ -0,0 +1,8 @@
+// takes an array as an argument so need a parameter and
+// returns 10 times the array length for 10 minutes per exercise
+function calculateWorkoutLength() {}
+
+// takes two arguments an array and a number of exercises and
+// returns a new array generated with random exercises based on
+// the number passed into the parameter
+function generateRandomExercises() {}
