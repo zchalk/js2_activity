afcfc4ad105a5b794d664aed6a36509d57acaf30
nmilfort <nmilfort1@gmail.com>
2022-01-12 16:12:36 -0500

    finished activity

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // takes an array as an argument so need a parameter and
 // returns 10 times the array length for 10 minutes per exercise
-function calculateWorkoutLength() {}
+function calculateWorkoutLength(array) {
+    return array.length * 10;
+}
 
 // takes two arguments an array and a number of exercises and
 // returns a new array generated with random exercises based on
 // the number passed into the parameter
-function generateRandomExercises() {}
+function generateRandomExercises(arr, num) {
+    const newArray = [];
+    for (let i=0; i<num; i++) {
+        let indx = Math.floor(Math.random()* arr.length);
+        newArray.push(arr[indx])
+    }
+    return newArray;
+}
